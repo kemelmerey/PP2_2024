b131b6ad59aff22f591f6b57ca8aad1c3ddb8740
Merey <kemelm2004@gmail.com>
2024-01-20 09:54:57 +0600

    added second exercise from comments

---
+++ lab 1/comm_ex2.py
@@ -0,0 +1,17 @@
+"""
+Exercise:
+Use a multiline string to make the a multiline comment:
+
+
+
+This is a comment
+written in 
+more than just one line
+
+"""
+#answer:
+"""
+This is a comment
+written in 
+more than just one line
+"""
