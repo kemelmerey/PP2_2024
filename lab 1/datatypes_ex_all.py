8b1c5a652fca550453ca4678f397956d08c33bab
Merey <kemelm2004@gmail.com>
2024-01-23 22:28:25 +0600

    added all exercise from data types

---
+++ lab 1/datatypes_ex_all.py
@@ -0,0 +1,72 @@
+#1
+"""
+Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = 5
+print(type(x))
+
+Answer: int
+
+"""
+#2
+"""
+Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = "Hello World"
+print(type(x))
+
+Answer: str
+"""
+#3
+"""Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = 20.5
+print(type(x))
+Answer: float
+"""
+#4
+"""
+Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+Answer: list
+"""
+#5
+"""Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+Answer: tuple
+"""
+#6
+"""
+Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+Answer: dict
+"""
+#7
+"""
+Exercise:
+The following code example would print the data type of x, what data type would that be?
+
+
+x = True
+print(type(x))
+Answer: bool
+"""
