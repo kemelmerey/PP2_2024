0d9263d44ab90ae25800f066e7161ed5cdb7be9c
Merey <kemelm2004@gmail.com>
2024-01-24 03:22:29 +0600

    added all example from escape characters

---
+++ lab 1/srt_escapechar_allexam.py
@@ -0,0 +1,9 @@
+#1
+"""
+You will get an error 
+if you use double quotes inside a string
+ that is surrounded by double quotes:
+ """
+txt = "We are the so-called "Vikings" from the north."
+#2
+txt = "We are the so-called \"Vikings\" from the north."
