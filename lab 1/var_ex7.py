1410eb50dde5e920108b681844d5cb98397a04c0
Merey <kemelm2004@gmail.com>
2024-01-23 21:05:39 +0600

    added 7th exercise from variables

---
+++ lab 1/var_ex7.py
@@ -0,0 +1,12 @@
+"""Exercise:
+Insert the correct keyword to make the variable x belong to the global scope.
+
+
+def myfunc():
+  
+ x
+  x = "fantastic"
+"""
+def myfunc():
+    global x
+    x="fantastic"
