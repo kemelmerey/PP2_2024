5c66e4928f5fdff54377f0d6e4ce5c0dc5470d24
Merey <kemelm2004@gmail.com>
2024-01-24 11:53:18 +0600

    added all exercises from boolean

---
+++ lab2/boolean_allex.py
@@ -0,0 +1,38 @@
+#1
+"""
+The statement below would print a Boolean value, which one?
+
+print(10 > 9)"""
+#Answer: True
+
+#2
+"""
+The statement below would print a Boolean value, which one?
+
+print(10 == 9)
+"""
+#Answer: False
+
+#3
+"""
+The statement below would print a Boolean value, which one?
+
+print(10 < 9)
+"""
+#Answer: False
+
+#4
+"""
+The statement below would print a Boolean value, which one?
+
+print(bool("abc"))
+"""
+#Answer: True
+
+#5
+"""
+The statement below would print a Boolean value, which one?
+
+print(bool(0))
+"""
+#Answer:False
