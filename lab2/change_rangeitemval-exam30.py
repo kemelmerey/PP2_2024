7e7cdc0520d83dc1784704904a303ea5d611f4c2
Merey <kemelm2004@gmail.com>
2024-01-27 10:32:47 +0600

    added 1st example from change a range of item values

---
+++ lab2/change_rangeitemval-exam30.py
@@ -0,0 +1,9 @@
+"""
+Change the values 
+"banana" and "cherry" 
+with the values 
+"blackcurrant" and "watermelon":
+"""
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
