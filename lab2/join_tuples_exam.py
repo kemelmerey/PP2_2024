89501b6ca0213d53cbe9fba95a1a5febabc340fd
Merey <kemelm2004@gmail.com>
2024-01-29 01:59:56 +0600

    added examples from join tuples

---
+++ lab2/join_tuples_exam.py
@@ -0,0 +1,18 @@
+#-----Join Two Tuples-----
+
+#Join two tuples:
+
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#-----Multiply Tuples-----
+
+#Multiply the fruits tuple by 2:
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
