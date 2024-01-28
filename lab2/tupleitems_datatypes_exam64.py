efa650c8344e243ad2f94c98eb06b897cd2c007b
Merey <kemelm2004@gmail.com>
2024-01-29 01:30:07 +0600

    added examples from tuple items - data types

---
+++ lab2/tupleitems_datatypes_exam64.py
@@ -0,0 +1,9 @@
+#String, int and boolean data types:
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+#A tuple with strings, integers and boolean values:
+
+tuple1 = ("abc", 34, True, 40, "male")
