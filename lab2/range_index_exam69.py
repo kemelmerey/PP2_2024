4317085b91e846a8ad19af54439f8ead3956c6fd
Merey <kemelm2004@gmail.com>
2024-01-29 01:40:05 +0600

    added examples from range of indexes

---
+++ lab2/range_index_exam69.py
@@ -0,0 +1,14 @@
+#Return the third, fourth, and fifth item:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+#This example returns the items from the beginning to, but NOT included, "kiwi":
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+#This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
