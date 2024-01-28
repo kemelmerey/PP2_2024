434b8700eb872a9e40744f483fd453f698a845d6
Merey <kemelm2004@gmail.com>
2024-01-28 23:40:10 +0600

    added examples from sort descending

---
+++ lab2/sort_desc_exam55.py
@@ -0,0 +1,9 @@
+#Sort the list descending
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Sort the list descending
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
