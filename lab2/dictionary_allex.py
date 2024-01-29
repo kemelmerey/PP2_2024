830eb46f9e4daf9ce897eac9774646fad9b13c8b
Merey <kemelm2004@gmail.com>
2024-01-29 15:07:10 +0600

    added exercises from dictionaries

---
+++ lab2/dictionary_allex.py
@@ -0,0 +1,61 @@
+#1
+"""
+Use the get method to print the value of the "model" key of the car dictionary.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(__)
+"""
+#Answer:car.get("model")
+
+#2
+"""
+Change the "year" value from 1964 to 2020.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+____=___
+"""
+#Answer:car["year"]=2020
+
+#3
+"""
+Add the key/value pair "color" : "red" to the car dictionary.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+"""
+#Answer:car["color"]="red"
+
+#4
+"""
+Use the pop method to remove "model" from the car dictionary.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+"""
+#Answer:car.pop("model")
+
+#5
+"""
+Use the clear method to empty the car dictionary.
+
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+"""
+#Answer:car.clear()
