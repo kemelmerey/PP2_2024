0d7c15482e3a5e6d6b3ca46cdbae1690ef9f654c
Merey <kemelm2004@gmail.com>
2024-01-29 14:25:31 +0600

    added examples from dictionaries

--- lab2/dictionaries_exam.py
+++ lab2/dictionaries_exam.py
@@ -31,3 +31,37 @@ thisdict = {
   "year": 2020
 }
 print(thisdict)
+
+#-----Dictionary Length-----
+
+#Print the number of items in the dictionary:
+
+print(len(thisdict))
+
+#-----Dictionary Items - Data Types-----
+
+#String, int, boolean, and list data types
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#-----type()-----<class 'dict'>
+
+#Print the data type of a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#-----The dict() Constructor-----
+
+#Using the dict() method to make a dictionary:
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
