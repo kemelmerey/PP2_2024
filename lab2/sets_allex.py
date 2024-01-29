89849535805162b46aae4a11678b03da3da34f3f
Merey <kemelm2004@gmail.com>
2024-01-29 14:17:14 +0600

    added all exercises from sets

---
+++ lab2/sets_allex.py
@@ -0,0 +1,42 @@
+#1
+"""
+Check if "apple" is present in the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+if "apple" __ fruits:
+  print("Yes, apple is a fruit!")
+"""
+#Answer: in
+
+#2
+"""
+Use the add method to add "orange" to the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+"""
+#Answer:fruits.add("orange")
+
+#3
+"""
+Use the correct method to add multiple items (more_fruits) to the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+"""
+#Answer: fruits.update(more_fruits)
+
+#4
+"""
+Use the remove method to remove "banana" from the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+"""
+#Answer:fruits.remove("banana")
+
+#5
+"""
+Use the discard method to remove "banana" from the fruits set.
+
+fruits = {"apple", "banana", "cherry"}
+"""
+#Answer:fruits.discard("banana")
